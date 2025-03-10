495190ca294bac981c31fb7aff3d9e5ed52e4c7d
TAATHub <20purpleblue10@gmail.com>
2025-03-10 22:49:43 +0900

    Initial commit

---
+++ DragRotateMagnify/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  DragRotateMagnify
+//
+//  Created by TAAT on 2025/03/10.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
